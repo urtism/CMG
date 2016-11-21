a8dddac68b6a3c7a117ddf604a779615b523616e
minime <minime@minime-CELSIUS-M460>
2016-11-21 10:04:00 +0100

    aggiunti i PATH agli scripts e ai tools

--- scripts/normalize_vcf.sh
+++ scripts/normalize_vcf.sh
@@ -3,6 +3,8 @@
 DIR=/home/minime/Scrivania/TEST/VCF
 DIR_OUT=/home/minime/Scrivania/TEST/VCF/OUT
 REFERENCE=/home/minime/reference/ucsc.hg19.fasta
+SCRIPTS_DIR=/home/minime/git/CMG/scripts
+TOOLS_DIR=/home/minime/tools
 
 mkdir -p $DIR_OUT
 
@@ -18,7 +20,7 @@ echo $filename_no_ext
 
 if [[ $filename =~ .*FreeBayes.* ]]
 	then
-		python /home/minime/git/CMG/scripts/header_fix.py \
+		python $SCRIPTS_DIR/header_fix.py \
 		-f $i \
 		-v F \
 		> $DIR_OUT/$filename_no_ext.fixed.vcf
@@ -28,13 +30,13 @@ elif [[ $filename =~ .*VarScan.* ]]
 		
 elif [[ $filename =~ .*GATK.* ]]
 	then
-		python /home/minime/git/CMG/scripts/header_fix.py \
+		python $SCRIPTS_DIR/header_fix.py \
 		-f $i \
 		-v G \
 		> $DIR_OUT/$filename_no_ext.fixed.vcf
 fi
 
-/home/minime/tools/bcftools-1.3.1/bcftools norm -m -both \
+$TOOLS_DIR/bcftools-1.3.1/bcftools norm -m -both \
 -f $REFERENCE \
 $DIR_OUT/$filename_no_ext.fixed.vcf \
 > $DIR_OUT/$filename_no_ext.split.vcf
