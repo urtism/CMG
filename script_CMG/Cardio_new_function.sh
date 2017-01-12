967fd1417acd314704b03fbcdd05c3f8882d81b7
jarvis <jarvis@jarvis-All-Series-Invalid-entry-length-16-Fixed-up-to-11>
2017-01-12 10:43:32 +0100

    dsdsd

--- script_CMG/Cardio_new_function.sh
+++ script_CMG/Cardio_new_function.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-cat ~/Scrivania/SCRIPT_PIPELINE/logo.txt 
+cat ~/Scrivania/SCRIPT_PIPELINE/logo_cmg.txt
 
 FASTQC=~/NGS_TOOLS/FastQC/fastqc
 BWA=~/NGS_TOOLS/bwa-0.7.15
 PICARD=~/NGS_TOOLS/picard-tools-2.7.1/picard.jar
 GATK=~/NGS_TOOLS/GATK/GenomeAnalysisTK.jar
 VARSCAN=~/NGS_TOOLS/VarScan/VarScan.v2.3.9.jar
+BCFTOOLS=~/NGS_TOOLS/bcftools-1.3.1/bcftools
 REF=~/NGS_TOOLS/hg19/ucsc.hg19.fasta
 MILLS=~/NGS_TOOLS/hg19/Mills_and_1000G_gold_standard.indels.hg19.sites.vcf
 DBSNP=~/NGS_TOOLS/hg19/dbsnp_138.hg19.vcf
@@ -31,32 +32,35 @@ VEP=~/NGS_TOOLS/ensembl-tools-release-86/scripts/variant_effect_predictor/
 VEPANN=~/NGS_TOOLS/ensembl-tools-release-86/scripts/variant_effect_predictor/variant_effect_predictor.pl
 VEPFILTER=~/NGS_TOOLS/ensembl-tools-release-86/scripts/variant_effect_predictor/filter_vep.pl
 
+DataRun=20151115
 
-	for filename in $PROCESSING/5_BQSR/*.bam
+cd $PROCESSING/5_BQSR/
+
+	for filename in *.bam
 	do
-
-		cat ~/Scrivania/SCRIPT_PIPELINE/logo_variant.txt
-		printf $"\n =========>	Sample ${filename%.*} => Variant Calling: Haplotype Caller\n\n"
-
-		java -Xmx64g -jar $GATK -T HaplotypeCaller \
-		-R $REF \
-		-I $PROCESSING/5_BQSR/${filename%.*}.bam \
-		-o $PROCESSING/6_Variant/GATK/${filename%.*}.g.vcf \
-		-ERC GVCF \
-		--doNotRunPhysicalPhasing \
-		--doNotRunPhysicalPhasing \
-		--heterozygosity 0.001 \
-		--indel_heterozygosity 1.25E-4 \
-		--maxReadsInRegionPerSample 50000 \
+ 
+ 		cat ~/Scrivania/SCRIPT_PIPELINE/logo_variant.txt
+ 		printf $"\n =========>	Sample ${filename%.*} => Variant Calling: Haplotype Caller\n\n"
+ 
+ 		java -Xmx64g -jar $GATK -T HaplotypeCaller \
+ 		-R $REF \
+ 		-I ${filename%.*}.bam \
+ 		-o ${filename%.*}.g.vcf \
+ 		-ERC GVCF \
+ 		--doNotRunPhysicalPhasing \
+ 		--doNotRunPhysicalPhasing \
+ 		--heterozygosity 0.001 \
+ 		--indel_heterozygosity 1.25E-4 \
+ 		--maxReadsInRegionPerSample 50000 \
 		--min_base_quality_score 10 \
 		--minReadsPerAlignmentStart 5 \
-		--max_alternate_alleles 6 \
-		#-bamout $PROCESSING/6_Variant/GATK/${filename%.*}.g.vcf.bam \
+ 		--max_alternate_alleles 6 \
 		-L $TARGET
-
-		printf $"\n =========>	Sample ${filename%.*} => Variant Calling: Haplotype Caller: DONE\n\n"
-
-
+		
+#		-bamout $PROCESSING/6_Variant/GATK/${filename%.*}.g.vcf.bam \
+ 
+ 		printf $"\n =========>	Sample ${filename%.*} => Variant Calling: Haplotype Caller: DONE\n\n"
+ 
 	done
 
 
@@ -67,23 +71,39 @@ VEPFILTER=~/NGS_TOOLS/ensembl-tools-release-86/scripts/variant_effect_predictor/
 
 
 
-
 	#Stampo tutti i file .g.vcf in una lista
-	ls $PROCESSING/6_Variant/GATK/*.g.vcf > samples.list #stampo il nome di tutti  files in una lista .list
+	
+	cd $PROCESSING/6_Variant/GATK/
+	
+	ls *.g.vcf > gvcf.list
 	cat ~/Scrivania/SCRIPT_PIPELINE/logo_multi.txt
 	echo $'\n =========>	Variant Calling: Genotype GVCF & Multi-sample variant calling\n\n'
 
 		java -jar -Xmx64g $GATK -T GenotypeGVCFs \
 		-R $REF \
-		-V:VCF samples.list \
-		-A HomopolymerRun \
-#		-newQual \
+		-V:VCF $PROCESSING/6_Variant/GATK/gvcf.list \
 		-o $PROCESSING/6_Variant/GATK/$DataRun\_Cardio_GATK.vcf
 
+	#	-A HomopolymerRun \
 	# Salvo nella cartella storage
-	#cp $PROCESSING/6_Variant/GATK/$DataRun\_Cardio_GATK.vcf $STORAGE/$Name_Dir
-
+	# cp $PROCESSING/6_Variant/GATK/$DataRun\_Cardio_GATK.vcf $STORAGE/$Name_Dir
 	echo $'\n\n =========>	Variant Calling: Genotype GVCF & Multi-sample variant calling: DONE\n'
+	cat ~/Scrivania/SCRIPT_PIPELINE/logo_cornice.txt
+	echo $'\n =========>	GATK vcf norm\n\n'
+
+	python ~/git/CMG/scripts/header_fix.py \
+	-f $PROCESSING/6_Variant/GATK/$DataRun\_Cardio_GATK.vcf \
+	-v G \
+	> $PROCESSING/6_Variant/GATK/NORM_GATK/$DataRun\_Cardio_GATK.fixed.vcf
+		
+	bcftools norm -m -both \
+	-f $REF \
+	$PROCESSING/6_Variant/GATK/NORM_GATK/$DataRun\_Cardio_GATK.fixed.vcf \
+	> $PROCESSING/6_Variant/GATK/NORM_GATK/$DataRun\_Cardio_GATK.split.vcf	
+
+	echo $'\n =========>	GATK vcf norm: DONE\n'
+	cat ~/Scrivania/SCRIPT_PIPELINE/logo_cornice.txt
+	echo $'\n =========>	Variant Calling with VarScan2\n\n'
 
 
 
@@ -97,9 +117,12 @@ VEPFILTER=~/NGS_TOOLS/ensembl-tools-release-86/scripts/variant_effect_predictor/
 
 
 
-	ls $PROCESSING/5_BQSR/*.bam > $PROCESSING/5_BQSR/Bam_list.txt
-	ls $PROCESSING/5_BQSR/*.bam > $PROCESSING/5_BQSR/Sample_list.txt
-	sed -i -e "s/.bam//g" $PROCESSING/5_BQSR/Sample_list.txt
+
+	cd $PROCESSING/5_BQSR/
+
+	ls *.bam > Bam_list.txt
+	ls *.bam > Sample_list.txt
+	sed -i -e "s/.bam//g" Sample_list.txt
 
 	/home/jarvis/NGS_TOOLS/freebayes/bin/freebayes -f $REF \
 	-L $PROCESSING/5_BQSR/Bam_list.txt \
@@ -120,33 +143,22 @@ VEPFILTER=~/NGS_TOOLS/ensembl-tools-release-86/scripts/variant_effect_predictor/
 
 	echo $'\n =========>	Variant Calling with FreeBayes: DONE\n'
 	cat ~/Scrivania/SCRIPT_PIPELINE/logo_cornice.txt
-	echo $'\n =========>	FreeBayes vcf manipulation for intersection\n\n'
+	echo $'\n =========>	FreeBayes vcf norm\n\n'
 
-	#Rimuovo le righe duplicate:
-	#awk '!a[$0]++' $PROCESSING/6_Variant/FreeBayes/$DataRun\_Cardio_FreeBayes.vcf > $PROCESSING/6_Variant/FreeBayes/$DataRun\_Cardio_FreeBayes_RD.vcf
-	
-	#Converto il vcf alla versione 4.2
-	#sed -i -e 's/fileformat=VCFv4.1/fileformat=VCFv4.2/g' $PROCESSING/6_Variant/FreeBayes/$DataRun\_Cardio_FreeBayes_RD.vcf
+	python ~/git/CMG/scripts/header_fix.py \
+	-f $PROCESSING/6_Variant/FreeBayes/$DataRun\_Cardio_FreeBayes.vcf \
+	-v G \
+	> $PROCESSING/6_Variant/FreeBayes/NORM_FREE/$DataRun\_Cardio_FreeBayes.fixed.vcf
+		
+	bcftools norm -m -both \
+	-f $REF \
+	$PROCESSING/6_Variant/FreeBayes/NORM_FREE/$DataRun\_Cardio_FreeBayes.fixed.vcf \
+	> $PROCESSING/6_Variant/FreeBayes/NORM_FREE/$DataRun\_Cardio_FreeBayes.split.vcf	
 
-	java -Xmx64g -jar $PICARD SortVcf \
-	I=$PROCESSING/6_Variant/FreeBayes/$DataRun\_Cardio_FreeBayes_RD.vcf \
-	O=$PROCESSING/6_Variant/FreeBayes/$DataRun\_Cardio_FreeBayes_Sort.vcf
-
-	cp $DataRun\_Cardio_FreeBayes_Sort.vcf $STORAGE/$Name_Dir/$DataRun\_Cardio_FreeBayes.vcf
-
-	#Copio il vcf nella cartella $PROCESSING/6_Variant/Intersect
-	cp $DataRun\_Cardio_FreeBayes_Sort.vcf $PROCESSING/6_Variant/Intersect/
-	bgzip $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_FreeBayes_Sort.vcf
-	tabix $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_FreeBayes_Sort.vcf.gz
-	rm $PROCESSING/6_Variant/FreeBayes/$DataRun\_Cardio_FreeBayes_RD.vcf
-	rm $PROCESSING/6_Variant/FreeBayes/$DataRun\_Cardio_FreeBayes_Sort.vcf
-	rm $PROCESSING/6_Variant/FreeBayes/$DataRun\_Cardio_FreeBayes.vcf
-	rm $PROCESSING/6_Variant/FreeBayes/$DataRun\_Cardio_FreeBayes_Sort.vcf.idx
-	rm $PROCESSING/5_BQSR/Sample_list.txt
-
-	echo $'\n =========>	FreeBayes vcf manipulation for intersection: DONE\n'
+	echo $'\n =========>	FreeBayes vcf norm: DONE\n'
 	cat ~/Scrivania/SCRIPT_PIPELINE/logo_cornice.txt
 	echo $'\n =========>	Variant Calling with VarScan2\n\n'
+
 
 
 
@@ -154,154 +166,153 @@ VEPFILTER=~/NGS_TOOLS/ensembl-tools-release-86/scripts/variant_effect_predictor/
 
 
 
-	samtools mpileup -B -q 1 -f $REF \
-	-l $TARGETCARDIOBED -b $PROCESSING/5_BQSR/Bam_list.txt > $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio.mpileup
-
-	cp $PROCESSING/5_BQSR/Bam_list.txt $PROCESSING/5_BQSR/Sample_list.txt
-	sed -i -e 's/.bam//g' $PROCESSING/5_BQSR/Sample_list.txt
-
-	java -jar -Xmx64g $VARSCAN mpileup2snp $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio.mpileup \
-	--min-coverage 10 \
-	--min-var-freq 0.20 \
-	--pvalue 0.05 \
-	--output-vcf 1 \
-	--vcf-sample-list $PROCESSING/5_BQSRSample_list.txt > $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_snp.vcf
-
-	java -jar -Xmx64g $VARSCAN mpileup2indel $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio.mpileup \
-	--min-coverage 10 \
-	--min-var-freq 0.10 \
-	--pvalue 0.1 \
-	--output-vcf 1 \
-	--vcf-sample-list $PROCESSING/5_BQSRSample_list.txt > $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_Indel.vcf
-
-	echo $'\n =========>	Variant Calling with VarScan2: DONE\n'
+ 	samtools mpileup -B -q 1 -f $REF \
+ 	-l $TARGETCARDIOBED -b $PROCESSING/5_BQSR/Bam_list.txt > $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio.mpileup
+ 
+ 	java -jar -Xmx64g $VARSCAN mpileup2snp $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio.mpileup \
+ 	--min-coverage 10 \
+ 	--min-var-freq 0.20 \
+ 	--pvalue 0.05 \
+ 	--output-vcf 1 \
+ 	--vcf-sample-list $PROCESSING/5_BQSR/Sample_list.txt > $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_snp.vcf
+ 
+ 	java -jar -Xmx64g $VARSCAN mpileup2indel $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio.mpileup \
+ 	--min-coverage 10 \
+ 	--min-var-freq 0.10 \
+ 	--pvalue 0.1 \
+ 	--output-vcf 1 \
+ 	--vcf-sample-list $PROCESSING/5_BQSR/Sample_list.txt > $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_Indel.vcf
+ 
+ 	echo $'\n =========>	Variant Calling with VarScan2: DONE\n'
+ 	cat ~/Scrivania/SCRIPT_PIPELINE/logo_cornice.txt
+ 	echo $'\n =========>	VarScan2 vcf merge and sort\n\n'
+ 
+ 	#Copio in Intersect
+ 	bgzip $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_snp.vcf
+ 	bgzip $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_Indel.vcf
+ 	tabix $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_snp.vcf.gz
+ 	tabix $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_Indel.vcf.gz
+ 
+ 	#Effettuo il merging dei file
+ 	
+ 	vcf-concat $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_snp.vcf.gz \
+ 	$PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_Indel.vcf.gz > $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_Merge.vcf
+ 	
+ 	vcf-sort -c $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_Merge.vcf > $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_Merge_Sort.vcf
+ 	mv $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_Merge_Sort.vcf $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan.vcf
+ 	rm /home/jarvis/Scrivania/NGS_ANALYSIS_TEST/PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_Merge.vcf
+ 	rm $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_snp.vcf.gz.tbi
+ 	rm $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_Indel.vcf.gz.tbi
+ 	rm $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_snp.vcf.gz
+ 	rm $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_Indel.vcf.gz
+ 	
+ 	echo $'\n =========>	VarScan2 vcf merge and sort: DONE\n'
 	cat ~/Scrivania/SCRIPT_PIPELINE/logo_cornice.txt
-	echo $'\n =========>	VarScan2 vcf manipulation for intersection\n\n'
-	
-	#Modifico la versione del vcf
-	sed -i -e 's/fileformat=VCFv4.1/fileformat=VCFv4.2/g' $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_Indel.vcf
-	sed -i -e 's/fileformat=VCFv4.1/fileformat=VCFv4.2/g' $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_snp.vcf
+	echo $'\n =========>	FreeBayes vcf norm\n\n'
 
-	#Copio in Intersect
-	cp $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_Indel.vcf $PROCESSING/6_Variant/Intersect/
-	cp $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_snp.vcf $PROCESSING/6_Variant/Intersect/
-	bgzip $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_snp.vcf
-	bgzip $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_Indel.vcf
-	tabix $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_snp.vcf.gz
-	tabix $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_Indel.vcf.gz
+	python ~/git/CMG/scripts/header_fix.py \
+	-f $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan.vcf \
+	-v G \
+	> $PROCESSING/6_Variant/VarScan/NORM_VARSCAN/$DataRun\_Cardio_VarScan.fixed.vcf
+		
+	bcftools norm -m -both \
+	-f $REF \
+	$PROCESSING/6_Variant/VarScan/NORM_VARSCAN/$DataRun\_Cardio_VarScan.fixed.vcf \
+	> $PROCESSING/6_Variant/VarScan/NORM_VARSCAN/$DataRun\_Cardio_VarScan.split.vcf	
 
-	#Effettuo il merging dei file
-	cd $PROCESSING/6_Variant/Intersect/
-	
-	vcf-concat $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_snp.vcf.gz \
-	$PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_Indel.vcf.gz > $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_Merge.vcf
-	
-	vcf-sort -c $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_Merge.vcf > $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_Merge_Sort.vcf
-
-	cp $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_Merge_Sort.vcf $STORAGE/$Name_Dir/$DataRun\_Cardio_VarScan.vcf
-
-	bgzip $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_Merge_Sort.vcf
-	tabix $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_Merge_Sort.vcf.gz
-
-	#Rimuovo i file inutili:
-	rm $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_snp.vcf.gz
-	rm $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_Indel.vcf.gz
-	rm $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_snp.vcf.gz.tbi
-	rm $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_Indel.vcf.gz.tbi
-	rm $PROCESSING/6_Variant/Intersect/$DataRun\_Cardio_VarScan_Merge.vcf
-	rm $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_Indel.vcf
-	rm $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio_VarScan_snp.vcf
-	rm $PROCESSING/6_Variant/VarScan/$DataRun\_Cardio.mpileup
-	
-	echo $'\n =========>	VarScan2 vcf manipulation for intersection: DONE\n'
+	echo $'\n =========>	FreeBayes vcf norm: DONE\n'
 	cat ~/Scrivania/SCRIPT_PIPELINE/logo_cornice.txt
-	echo $'\n =========>	GATK + FreeBayes + VarScan2 INTERSECTION\n\n'
-
+	echo $'\n =========>	Variant Calling with VarScan2\n\n'
 
 
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::    GATK ANNOTATION     ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-
-
-	printf "\n\n"
-	cat ~/Scrivania/SCRIPT_PIPELINE/logo_annotation.txt
-	printf $"\n\n\n"
-
-	cd $VEP
-
-	perl $VEPANN -i $PROCESSING/6_Variant/GATK/$DataRun\_Cardio_GATK_Filter.vcf \
-	-o $PROCESSING/8_Annotation/$DataRun\_Cardio_GATK_ANN.vcf \
-	--stats_file $PROCESSING/8_Annotation/$DataRun\_Cardio_GATK_ANN.html \
-	--cache \
-	--assembly GRCh37 \
-	--offline \
-	--force_overwrite \
-	-v \
-	--fork 10 \
-	--variant_class \
-	--sift b \
-	--poly b \
-	--vcf_info_field ANN \
-	--hgvs \
-	--protein \
-	--canonical \
-	--check_existing \
-	--gmaf \
-	--pubmed \
-	--species homo_sapiens \
-	--failed 1 \
-	--vcf
-
-	echo $'\n =========>	GATK ANNOTATION: DONE\n'
-	cat ~/Scrivania/SCRIPT_PIPELINE/logo_cornice.txt
-	echo $'\n =========>	INTERSECTION ANNOTATION\n\n'
-
-	rm $PROCESSING/6_Variant/GATK/$DataRun\_Cardio_GATK_Filter.vcf
-	rm $PROCESSING/6_Variant/GATK/$DataRun\_Cardio_GATK_Filter.vcf.idx
-	cp $PROCESSING/8_Annotation/$DataRun\_Cardio_GATK_ANN.vcf $STORAGE/$Name_Dir
-	printf "\n${DataRun}_Cardio_GATK_ANN.vcf : contiene il file ${DataRun}_Cardio_GATK_Filter.vcf annotato\n" >> $STORAGE/$Name_Dir/README.txt
-	mv $PROCESSING/8_Annotation/$DataRun\_Cardio_GATK_ANN.html $STORAGE/$Name_Dir
-	printf "\n${DataRun}_Cardio_GATK_ANN.html : contiene le statistiche del file ${DataRun}_Cardio_GATK_Filter.vcf\n" >> $STORAGE/$Name_Dir/README.txt
+	#Partire da questi
+	#$PROCESSING/6_Variant/VarScan/NORM_VARSCAN/$DataRun\_Cardio_VarScan.split.vcf
+	#$PROCESSING/6_Variant/FreeBayes/NORM_FREE/$DataRun\_Cardio_FreeBayes.split.vcf
+	#$PROCESSING/6_Variant/GATK/NORM_GATK/$DataRun\_Cardio_GATK.split.vcf	
 
 
 
-#:::::::::::::::::::::::::::::::::::::::::::::::::::    INTERSECTION  ANNOTATION     ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-
-
-	cd $VEP
-
-	perl $VEPANN -i $PROCESSING/7_Filter/$DataRun\_Cardio_Total_Intersect_Sort.vcf \
-	-o $PROCESSING/8_Annotation/$DataRun\_Cardio_Total_Intersect_ANN.vcf \
-	--stats_file $PROCESSING/8_Annotation/$DataRun\_Cardio_Total_Intersect_ANN.html \
-	--cache \
-	--assembly GRCh37 \
-	--offline \
-	--force_overwrite \
-	-v \
-	--fork 10 \
-	--variant_class \
-	--sift b \
-	--poly b \
-	--vcf_info_field ANN \
-	--hgvs \
-	--protein \
-	--canonical \
-	--check_existing \
-	--gmaf \
-	--pubmed \
-	--species homo_sapiens \
-	--failed 1 \
-	--vcf
-
-	rm $PROCESSING/7_Filter/$DataRun\_Cardio_Total_Intersect_Sort.vcf
-	cp $PROCESSING/8_Annotation/$DataRun\_Cardio_Total_Intersect_ANN.vcf $STORAGE/$Name_Dir/$DataRun\_Cardio_Intersect_ANN.vcf
-	printf "\n${DataRun}_Cardio_Intersect_ANN.vcf : contiene il file ${DataRun}_Cardio_Intersect.vcf annotato\n" >> $STORAGE/$Name_Dir/README.txt
-	mv $PROCESSING/8_Annotation/$DataRun\_Cardio_Total_Intersect_ANN.html $STORAGE/$Name_Dir
-	printf "\n${DataRun}_Cardio_Intersect_ANN.html : contiene le statistiche del file ${DataRun}_Cardio_Intersect_ANN.vcf\n" >> \
-	$STORAGE/$Name_Dir/README.txt
-
-	echo $'\n =========>	INTERSECTION ANNOTATION: DONE\n'
-	cat ~/Scrivania/SCRIPT_PIPELINE/logo_cornice.txt
-	echo $'\n =========>	FILE MODIFICATION FOR CSV FORMAT \n\n'
+# 	printf "\n\n"
+# 	cat ~/Scrivania/SCRIPT_PIPELINE/logo_annotation.txt
+# 	printf $"\n\n\n"
+ 
+# 	cd $VEP
+# 
+# 	perl $VEPANN -i $PROCESSING/6_Variant/GATK/$DataRun\_Cardio_GATK_Filter.vcf \
+# 	-o $PROCESSING/8_Annotation/$DataRun\_Cardio_GATK_ANN.vcf \
+# 	--stats_file $PROCESSING/8_Annotation/$DataRun\_Cardio_GATK_ANN.html \
+# 	--cache \
+# 	--assembly GRCh37 \
+# 	--offline \
+# 	--force_overwrite \
+# 	-v \
+# 	--fork 10 \
+# 	--variant_class \
+# 	--sift b \
+# 	--poly b \
+# 	--vcf_info_field ANN \
+# 	--hgvs \
+# 	--protein \
+# 	--canonical \
+# 	--check_existing \
+# 	--gmaf \
+# 	--pubmed \
+# 	--species homo_sapiens \
+# 	--failed 1 \
+# 	--vcf
+# 
+# 	echo $'\n =========>	GATK ANNOTATION: DONE\n'
+# 	cat ~/Scrivania/SCRIPT_PIPELINE/logo_cornice.txt
+# 	echo $'\n =========>	INTERSECTION ANNOTATION\n\n'
+# 
+# 	rm $PROCESSING/6_Variant/GATK/$DataRun\_Cardio_GATK_Filter.vcf
+# 	rm $PROCESSING/6_Variant/GATK/$DataRun\_Cardio_GATK_Filter.vcf.idx
+# 	cp $PROCESSING/8_Annotation/$DataRun\_Cardio_GATK_ANN.vcf $STORAGE/$Name_Dir
+# 	printf "\n${DataRun}_Cardio_GATK_ANN.vcf : contiene il file ${DataRun}_Cardio_GATK_Filter.vcf annotato\n" >> $STORAGE/$Name_Dir/README.txt
+# 	mv $PROCESSING/8_Annotation/$DataRun\_Cardio_GATK_ANN.html $STORAGE/$Name_Dir
+# 	printf "\n${DataRun}_Cardio_GATK_ANN.html : contiene le statistiche del file ${DataRun}_Cardio_GATK_Filter.vcf\n" >> $STORAGE/$Name_Dir/README.txt
+# 
+# 
+# 
+# #:::::::::::::::::::::::::::::::::::::::::::::::::::    INTERSECTION  ANNOTATION     ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# 
+# 
+# 
+# 	cd $VEP
+# 
+# 	perl $VEPANN -i $PROCESSING/7_Filter/$DataRun\_Cardio_Total_Intersect_Sort.vcf \
+# 	-o $PROCESSING/8_Annotation/$DataRun\_Cardio_Total_Intersect_ANN.vcf \
+# 	--stats_file $PROCESSING/8_Annotation/$DataRun\_Cardio_Total_Intersect_ANN.html \
+# 	--cache \
+# 	--assembly GRCh37 \
+# 	--offline \
+# 	--force_overwrite \
+# 	-v \
+# 	--fork 10 \
+# 	--variant_class \
+# 	--sift b \
+# 	--poly b \
+# 	--vcf_info_field ANN \
+# 	--hgvs \
+# 	--protein \
+# 	--canonical \
+# 	--check_existing \
+# 	--gmaf \
+# 	--pubmed \
+# 	--species homo_sapiens \
+# 	--failed 1 \
+# 	--vcf
+# 
+# 	rm $PROCESSING/7_Filter/$DataRun\_Cardio_Total_Intersect_Sort.vcf
+# 	cp $PROCESSING/8_Annotation/$DataRun\_Cardio_Total_Intersect_ANN.vcf $STORAGE/$Name_Dir/$DataRun\_Cardio_Intersect_ANN.vcf
+# 	printf "\n${DataRun}_Cardio_Intersect_ANN.vcf : contiene il file ${DataRun}_Cardio_Intersect.vcf annotato\n" >> $STORAGE/$Name_Dir/README.txt
+# 	mv $PROCESSING/8_Annotation/$DataRun\_Cardio_Total_Intersect_ANN.html $STORAGE/$Name_Dir
+# 	printf "\n${DataRun}_Cardio_Intersect_ANN.html : contiene le statistiche del file ${DataRun}_Cardio_Intersect_ANN.vcf\n" >> \
+# 	$STORAGE/$Name_Dir/README.txt
+# 
+# 	echo $'\n =========>	INTERSECTION ANNOTATION: DONE\n'
+# 	cat ~/Scrivania/SCRIPT_PIPELINE/logo_cornice.txt
+# 	echo $'\n =========>	FILE MODIFICATION FOR CSV FORMAT \n\n'
 
