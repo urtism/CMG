0866f3455486e1f2c2c8e4d5cfd1cc4864f18377
jarvis <jarvis@jarvis.sanmatteo.loc>
2017-03-16 12:17:13 +0100

    a

--- SCRIPT_CMG/SCRIPT_PYTHON/Database_Annotation.py
+++ SCRIPT_CMG/SCRIPT_PYTHON/Database_Annotation.py
@@ -400,9 +400,8 @@ def main():
 	parser.add_argument('-gerp','--gerp',default=None,help="path database Gerp++. Utilizzo: path_to_GERP_folder,RS_Score")
 	parser.add_argument('-phastCons','--phastCons',default=None,help="path database phastCons. Utilizzo: path_to_folder,species,species ---> es: ~/phastCons/,primate,vertebrate,")
 	parser.add_argument('-phyloP','--phyloP',default=None,help="path database phyloP46way_placental. Utilizzo: path_to_folder,species ---> es: ~/phyloP/placental,vertebrate,")
+	parser.add_argument('-COSMIC','--cosmic',default=None,help="path database COSMIC es: ~/COSMIC/valore_da_aggiungere,")	
 	parser.add_argument('-P','--pannello',help="Specificare il pannello utilizzato nell'analisi: Cancer | Cardio | Exome | BRCA")
-
-
 	parser.add_argument('-o','--outfile',help="file di output tab delimited")
 
 	global opts
@@ -439,6 +438,9 @@ def main():
 
 		if opts.phyloP:
 			line = Add_phyloP(line,header_tsv,pan)
+
+		if opts.cosmic:
+			line = Add_cosmic(line,header_tsv,pan)
 
 		print line
 
